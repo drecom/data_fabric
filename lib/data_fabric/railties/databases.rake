4fb74ae0ea1230d99ba7bc3cbd1ea1f56091c92e
gussan <egussan@gmail.com>
2014-05-26 18:36:01 +0900

    Respect default database tasks

--- lib/data_fabric/railties/databases.rake
+++ lib/data_fabric/railties/databases.rake
@@ -4,33 +4,26 @@ require 'data_fabric'
 require 'data_fabric/sharded_database_tasks'
 
 db_namespace = namespace :db do
-  Rake.application.lookup('db:create').clear
-  desc 'Create the database from DATABASE_URL or config/database.yml for the current Rails.env (use db:create:all to create all dbs in the config)'
+  desc 'Create data_fabric databases config/database.yml for the current Rails.env'
   task :create => [:load_config] do
-    if ENV['DATABASE_URL']
-      ActiveRecord::Tasks::DatabaseTasks.create_database_url
-    else
-      ActiveRecord::Tasks::DatabaseTasks.create_current_cluster
+    unless ENV['DATABASE_URL']
+      ActiveRecord::Tasks::DatabaseTasks.create_current_data_fabric_cluster
     end
   end
 
-  Rake.application.lookup('db:drop').clear
-  desc 'Drops the database using DATABASE_URL or the current Rails.env (use db:drop:all to drop all databases)'
+  desc 'Drops data_fabric databases for the current Rails.env (use db:drop:all to drop all databases)'
   task :drop => [:load_config] do
-    if ENV['DATABASE_URL']
-      ActiveRecord::Tasks::DatabaseTasks.drop_database_url
-    else
-      ActiveRecord::Tasks::DatabaseTasks.drop_current_cluster
+    unless ENV['DATABASE_URL']
+      ActiveRecord::Tasks::DatabaseTasks.drop_current_data_fabric_cluster
     end
   end
 
-  Rake.application.lookup('db:migrate').clear
-  desc "Migrate the database (options: VERSION=x, VERBOSE=false, SCOPE=blog)."
+  desc "Migrate data_fabric databases (options: VERSION=x, VERBOSE=false, SCOPE=blog)."
   task :migrate => [:environment, :load_config] do
     ActiveRecord::Migration.verbose = ENV["VERBOSE"] ? ENV["VERBOSE"] == "true" : true
 
-    ActiveRecord::Tasks::DatabaseTasks.each_current_cluster_connected do |configuration|
-      puts "*** Migrating database: #{configuration['database']}"
+    ActiveRecord::Tasks::DatabaseTasks.each_current_data_fabric_cluster_connected do |configuration|
+      puts "[data_fabric] *** Migrating database: #{configuration['database']}"
       ActiveRecord::Migrator.migrate(ActiveRecord::Migrator.migrations_paths, ENV["VERSION"] ? ENV["VERSION"].to_i : nil) do |migration|
         ENV["SCOPE"].blank? || (ENV["SCOPE"] == migration.scope)
       end
--- lib/data_fabric/sharded_database_tasks.rb
+++ lib/data_fabric/sharded_database_tasks.rb
@@ -3,26 +3,26 @@ require 'active_record/tasks/database_tasks'
 module ActiveRecord
   module Tasks
     module DatabaseTasks
-      attr_accessor :exclude_shard_regexp
-      self.exclude_shard_regexp = /(master|slave|standby)/
+      attr_accessor :exclude_data_fabric_shard_regexp
+      self.exclude_data_fabric_shard_regexp = /(master|slave|standby)/
 
-      def create_current_cluster(environment = env)
-        each_current_cluster_configuration(environment) { |configuration|
-          puts "*** executing to database: #{configuration['database']}"
+      def create_current_data_fabric_cluster(environment = env)
+        each_current_data_fabric_cluster_configuration(environment) { |configuration|
+          puts "[data_fabric] *** executing to database: #{configuration['database']}"
           create configuration
         }
         ActiveRecord::Base.establish_connection environment
       end
 
-      def drop_current_cluster(environment = env)
-        each_current_cluster_configuration(environment) { |configuration|
-          puts "*** executing to database: #{configuration['database']}"
+      def drop_current_data_fabric_cluster(environment = env)
+        each_current_data_fabric_cluster_configuration(environment) { |configuration|
+          puts "[data_fabric] *** executing to database: #{configuration['database']}"
           drop configuration
         }
       end
 
-      def each_current_cluster_connected(environment = env, exclude_pattern = exclude_shard_regexp)
-        each_current_cluster_configuration(environment, exclude_pattern) do |configuration|
+      def each_current_data_fabric_cluster_connected(environment = env, exclude_pattern = exclude_data_fabric_shard_regexp)
+        each_current_data_fabric_cluster_configuration(environment, exclude_pattern) do |configuration|
           ActiveRecord::Base.clear_active_connections!
           ActiveRecord::Base.establish_connection(configuration)
           yield configuration
@@ -33,12 +33,14 @@ module ActiveRecord
 
       private
 
-      def each_current_cluster_configuration(environment, exclude_pattern = exclude_shard_regexp)
+      def each_current_data_fabric_cluster_configuration(environment, exclude_pattern = exclude_data_fabric_shard_regexp)
         environments = [environment]
         environments << 'test' if environment == 'development'
         regexp = Regexp.compile("(#{environments.map {|e| Regexp.escape(e)}.join('|')})")
 
-        configurations = ActiveRecord::Base.configurations.select {|k,v| k =~ regexp and (!exclude_pattern or k !~ exclude_pattern) }.values.uniq
+        configurations = ActiveRecord::Base.configurations.select {|k,v|
+          k =~ regexp and (!exclude_pattern or k !~ exclude_pattern) and !environments.include?(k)
+        }.values.uniq
         configurations.compact.each do |configuration|
           yield configuration unless configuration['database'].blank?
         end
